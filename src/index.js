08f6aa2cb4044f0ab5b751dfc814eae216588abb
lerou7777 <sousawesleylopes@gmail.com>
2026-01-17 01:46:19 -0300

    deploy inicial do backend

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Folly Backend ONLINE 🚀");
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port", PORT);
+});
